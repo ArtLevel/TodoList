1852fbaf514028ff4b1e5b4d0686beaa8fd9eb57
ArtLevel <algitIT@rambler.ru>
2023-08-30 13:33:47 +0300

    Added MUI components

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -1,42 +1,45 @@
-import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import { Button, TextField } from '@mui/material'
+import React, { ChangeEvent, KeyboardEvent, useState } from 'react'
 
 type AddItemFormPropsType = {
-    addItem: (title: string) => void
+	addItem: (title: string) => void
 }
 
 export function AddItemForm(props: AddItemFormPropsType) {
 
-    let [title, setTitle] = useState("")
-    let [error, setError] = useState<string | null>(null)
+	let [title, setTitle] = useState('')
+	let [error, setError] = useState<string | null>(null)
 
-    const addItem = () => {
-        if (title.trim() !== "") {
-            props.addItem(title);
-            setTitle("");
-        } else {
-            setError("Title is required");
-        }
-    }
+	const addItem = () => {
+		if (title.trim() !== '') {
+			props.addItem(title)
+			setTitle('')
+		} else {
+			setError('Title is required')
+		}
+	}
 
-    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setTitle(e.currentTarget.value)
-    }
+	const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+		setTitle(e.currentTarget.value)
+	}
 
-    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-        setError(null);
-        if (e.charCode === 13) {
-            addItem();
-        }
-    }
+	const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+		setError(null)
+		if (e.charCode === 13) {
+			addItem()
+		}
+	}
 
-    return <div>
-        <input value={title}
-               onChange={onChangeHandler}
-               onKeyPress={onKeyPressHandler}
-               className={error ? "error" : ""}
-        />
-        <button onClick={addItem}>+</button>
+	return <div>
+		<TextField
+			size='small'
+			value={title}
+			onChange={onChangeHandler}
+			onKeyPress={onKeyPressHandler}
+			className={error ? 'error' : ''}
+		/>
+		<Button onClick={addItem} variant='contained' color='primary'>+</Button>
 
-        {error && <div className="error-message">{error}</div>}
-    </div>
+		{error && <div className='error-message'>{error}</div>}
+	</div>
 }
