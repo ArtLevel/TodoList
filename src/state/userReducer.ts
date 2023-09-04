e3055d1954953caa199c223ef0f4067349453275
ArtLevel <algitIT@rambler.ru>
2023-09-04 10:49:24 +0300

    Create test for UserReducer

--- src/state/userReducer.ts
+++ src/state/userReducer.ts
@@ -9,10 +9,15 @@ type ActionType = {
 	[key: string]: any
 }
 
-export const userReducer = (state: any, action: any) => {
+export const userReducer = (state: StateType, action: ActionType) => {
 	switch (action.type) {
-		case 'BLABLA1': {
-
+		case 'INCREMENT-AGE': {
+			state.age += 1
+			return state
+		}
+		case 'INCREMENT-CHILDREN-COUNT': {
+			state.childrenCount += 1
+			return state
 		}
 		default:
 			throw new Error(`I don't understand this action type`)
