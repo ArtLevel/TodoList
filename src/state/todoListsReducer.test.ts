e9ccf50465ee588c199804f0d6458991bf5355ae
ArtLevel <algitIT@rambler.ru>
2023-09-04 11:26:33 +0300

    Test todoListsReducer

--- src/state/todoListsReducer.test.ts
+++ src/state/todoListsReducer.test.ts
@@ -1,5 +1,5 @@
 import { v1 } from 'uuid'
-import { TodolistType } from '../App'
+import { FilterValuesType, TodolistType } from '../App'
 import { todoListsReducer } from './todoListsReducer'
 
 test('correct todoList should be removed', () => {
@@ -33,4 +33,49 @@ test('correct todoList should be removed', () => {
 	expect(endState.length).toBe(3)
 	expect(endState[2].title).toBe(newTodoListTitle)
 	expect(endState[2].filter).toBe('all')
+})
+
+test('correct todoList should change its name', () => {
+	const todoListId1 = v1()
+	const todoListId2 = v1()
+
+	const newTodoListTitle = 'New TodoList'
+
+	const startState: TodolistType[] = [
+		{ id: todoListId1, title: 'What to learn', filter: 'all' },
+		{ id: todoListId2, title: 'What to buy', filter: 'all' }
+	]
+
+	const action = {
+		type: 'CHANGE-TODOLIST-TITLE',
+		id: todoListId2,
+		title: newTodoListTitle
+	}
+
+	const endState = todoListsReducer(startState, action)
+
+	expect(endState[0].title).toBe('What to learn')
+	expect(endState[1].title).toBe('New TodoList')
+})
+test('correct filter of todoList should be changed', () => {
+	const todoListId1 = v1()
+	const todoListId2 = v1()
+
+	const newFilter: FilterValuesType = 'completed'
+
+	const startState: TodolistType[] = [
+		{ id: todoListId1, title: 'What to learn', filter: 'all' },
+		{ id: todoListId2, title: 'What to buy', filter: 'all' }
+	]
+
+	const action = {
+		type: 'CHANGE-TODOLIST-FILTER',
+		id: todoListId2,
+		filter: newFilter
+	}
+
+	const endState = todoListsReducer(startState, action)
+
+	expect(endState[0].filter).toBe('all')
+	expect(endState[1].filter).toBe(newFilter)
 })
--- src/state/todoListsReducer.ts
+++ src/state/todoListsReducer.ts
@@ -8,13 +8,15 @@ type ActionType = {
 
 export const todoListsReducer = (state: TodolistType[], action: ActionType): TodolistType[] => {
 	switch (action.type) {
-
 		case 'REMOVE-TODOLIST':
 			return state.filter(t => t.id !== action.id)
 		case 'ADD-TODOLIST': {
 			return [...state, { id: v1(), title: action.title, filter: 'all' }]
 		}
-
+		case 'CHANGE-TODOLIST-TITLE':
+			return state.map(t => t.id === action.id ? { ...t, title: action.title } : t)
+		case 'CHANGE-TODOLIST-FILTER':
+			return state.map(t => t.id === action.id ? { ...t, filter: action.filter } : t)
 		default:
 			throw new Error(`I don't understand this action type`)
 	}
