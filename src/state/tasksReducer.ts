e0ca0091445a3e7745b350eff944d4099b7ec10b
ArtLevel <algitIT@rambler.ru>
2023-09-05 14:05:13 +0300

    Complete unit test and reducer

---
+++ src/state/tasksReducer.ts
@@ -0,0 +1,66 @@
+import { FilterValuesType, TodolistType } from '../App'
+import { v1 } from 'uuid'
+
+type RemoveTodoListActionType = {
+	type: 'REMOVE-TODOLIST'
+	id: string
+}
+
+type AddTodoListActionType = {
+	type: 'ADD-TODOLIST'
+	title: string
+}
+
+type ChangeTodoListTitleActionType = {
+	type: 'CHANGE-TODOLIST-TITLE'
+	id: string
+	title: string
+}
+
+type ChangeTodoListFilterActionType = {
+	type: 'CHANGE-TODOLIST-FILTER'
+	id: string
+	filter: FilterValuesType
+}
+
+export type ActionsType =
+	RemoveTodoListActionType
+	| AddTodoListActionType
+	| ChangeTodoListTitleActionType
+	| ChangeTodoListFilterActionType
+export const todoListsReducer = (state: TodolistType[], action: ActionsType): TodolistType[] => {
+	switch (action.type) {
+		case 'REMOVE-TODOLIST':
+			return state.filter(t => t.id !== action.id)
+		case 'ADD-TODOLIST':
+			return [...state, { id: v1(), title: action.title, filter: 'all' }]
+		case 'CHANGE-TODOLIST-TITLE':
+			return state.map(t => t.id === action.id ? { ...t, title: action.title } : t)
+		case 'CHANGE-TODOLIST-FILTER':
+			return state.map(t => t.id === action.id ? { ...t, filter: action.filter } : t)
+		default:
+			throw new Error(`I don't understand this action type`)
+	}
+}
+
+export const removeTodoListAC = (todoListId: string): RemoveTodoListActionType => ({
+	type: 'REMOVE-TODOLIST',
+	id: todoListId
+})
+
+export const addTodoListAC = (title: string): AddTodoListActionType => ({
+	type: 'ADD-TODOLIST',
+	title
+})
+
+export const changeTodoListTitleAC = (id: string, title: string): ChangeTodoListTitleActionType => ({
+	type: 'CHANGE-TODOLIST-TITLE',
+	id,
+	title
+})
+
+export const changeTodoListFilterAC = (id: string, filter: FilterValuesType): ChangeTodoListFilterActionType => ({
+	type: 'CHANGE-TODOLIST-FILTER',
+	id,
+	filter
+})
