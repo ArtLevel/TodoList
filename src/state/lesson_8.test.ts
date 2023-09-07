7c584eea9b12e9d4fe22b605a368e847b98f3f3b
ArtLevel <algitIT@rambler.ru>
2023-09-07 20:36:54 +0300

    Exam complete

--- src/state/lesson_8.test.ts
+++ src/state/lesson_8.test.ts
@@ -1,36 +1,45 @@
-import {getBanknoteList, getSum, getTriangleType, isEvenIndexSumGreater, getSquarePositiveIntegers, sum, sumFirstNumbers} from "./lesson_8";
+import {
+	getBanknoteList,
+	getSquarePositiveIntegers,
+	getSum,
+	getTriangleType,
+	isEvenIndexSumGreater,
+	sum,
+	sumFirstNumbers
+} from './lesson_8'
 
-test("sum", () => {
+test('sum', () => {
 	expect(sum(3, 5, 7, 6, 4, 9)).toBe(34)
 	expect(sum(1, 1, 1, 6)).toBe(9)
 })
 
-test("get Triangle Type", ()=> {
-	expect(getTriangleType(1, 1, 1)).toBe("10")
-	expect(getTriangleType(2, 3, 3)).toBe("01")
-	expect(getTriangleType(3, 3, 2)).toBe("01")
-	expect(getTriangleType(4, 5, 3)).toBe("11")
-	expect(getTriangleType(10, 2, 2)).toBe("00")
+test('get Triangle Type', () => {
+	expect(getTriangleType(1, 1, 1)).toBe('10')
+	expect(getTriangleType(2, 3, 3)).toBe('01')
+	expect(getTriangleType(3, 3, 2)).toBe('01')
+	expect(getTriangleType(4, 5, 3)).toBe('11')
+	expect(getTriangleType(10, 2, 2)).toBe('00')
 })
 
-test("get Sum ", ()=> {
+test('get Sum ', () => {
 	expect(getSum(1000)).toBe(1)
 	expect(getSum(0)).toBe(0)
 	expect(getSum(1234)).toBe(10)
 	expect(getSum(9999)).toBe(36)
 })
-
-test("is Even Sum Greater", ()=> {
+//
+test('is Even Sum Greater', () => {
 	expect(isEvenIndexSumGreater([1, 100, 2, 200])).toBe(false)
 	expect(isEvenIndexSumGreater([100, 1, 200, 2])).toBe(true)
 	expect(isEvenIndexSumGreater([100, 1, 200, 2, 300, 4])).toBe(true)
 	expect(isEvenIndexSumGreater([100, 1, 200, 2, 4])).toBe(true)
 })
-
-test("get Square Only Of Positive Integers", () => {
+//
+test('get Square Only Of Positive Integers', () => {
 	const array = [4, 5.6, -9.8, 3.14, 10, 6, 8.34, -2]
 	const len = array.length
 	const result = getSquarePositiveIntegers(array)
+
 	expect(result === array).toBe(false)
 	expect(array.length).toBe(len)
 	expect(result.length).toBe(3)
@@ -38,14 +47,14 @@ test("get Square Only Of Positive Integers", () => {
 	expect(result[1]).toBe(100)
 	expect(result[2]).toBe(36)
 })
-
-test("sum of first N numbers", () => {
+//
+test('sum of first N numbers', () => {
 	expect(sumFirstNumbers(0)).toBe(0)
 	expect(sumFirstNumbers(4)).toBe(10)
 	expect(sumFirstNumbers(10)).toBe(55)
 })
-
-test("get banknote list", ()=> {
+//
+test('get banknote list', () => {
 	// надо бы проверять длинну резалтов и их сумму
 	const result2500 = getBanknoteList(2500)
 	const result23 = getBanknoteList(23)
--- src/state/lesson_8.ts
+++ src/state/lesson_8.ts
@@ -8,58 +8,50 @@ export const sum = (...nums: number[]): number => nums.reduce((acc, num) => acc 
 //  - "10", если треугольник равносторонний,
 //  - "01", если треугольник равнобедренный,
 //  - "11", если треугольник обычный,
-//  - "00", если такого треугольника не существует.
+//  - "00", если такого треугольника не существует
 
 export function getTriangleType(a: number, b: number, c: number): string {
-    if(a === b && a === c && b === c) return '10'
-    if(a === b || a === c || b === c) return '01'
-    if(a !== b || a !== c || b !== c) return '11'
-    return "00"
+	if (a + b < c || a + c < b || b + c < a) return '00'
+	if (a === b && a === c && b === c) return '10'
+	if (a === b || a === c || b === c) return '01'
+	return '11'
 }
 
 // 3. Функция getSum принимает параметром целое число и возвращает
 // сумму цифр этого числа
 
-export function getSum(number: number): number{
-    //...здесь пишем код.
-    // В return стоит "заглушка", чтоб typescript не ругался
-    return 123
-}
-
+export const getSum = (number: number): number => String(number).split('').reduce((acc, str) => acc + Number(str), 0)
 
 // 4. Функция isEvenIndexSumGreater принимает  параметром массив чисел.
 // Если сумма чисел с чётными ИНДЕКСАМИ!!! (0 как чётный индекс) больше
 // суммы чисел с нечётными ИНДЕКСАМИ!!!, то функция возвращает true.
 // В противном случае - false.
 
-export const isEvenIndexSumGreater = (arr: Array<number>): boolean => {
-    //...здесь пишем код.
-    // В return стоит "заглушка", чтоб typescript не ругался
-    return true
+export const isEvenIndexSumGreater = (arr: number[]): boolean => {
+	let evenIndexSum = 0
+	let oddIndexSum = 0
+
+	arr.forEach((el, i) => i % 2 === 0 ? evenIndexSum += el : oddIndexSum += el)
+
+	return evenIndexSum > oddIndexSum
 }
 
-// 5. Функция getSquarePositiveIntegers принимает параметром массив чисел и возвращает новый массив. 
+// 5. Функция getSquarePositiveIntegers принимает параметром массив чисел и возвращает новый массив.
 // Новый массив состоит из квадратов целых положительных чисел, котрые являются элементами исходгого массива.
 // Исходный массив не мутирует.
 
-
-export function getSquarePositiveIntegers(array: Array<number>): Array<number> {
-    //...здесь пишем код.
-    // В return стоит "заглушка", чтоб typescript не ругался
-    return []
-}
+export const getSquarePositiveIntegers = (array: number[]): number[] => array.filter(el => Number.isInteger(el) && el > 0).map(el => Math.pow(el, 2))
 
 // 6. Функция принимает параметром целое не отрицательное число N и возвращает сумму всех чисел от 0 до N включительно
 // Попробуйте реализовать функцию без использования перебирающих методов.
 
-export function sumFirstNumbers(N: number): number {
-    //...здесь пишем код.
-    // В return стоит "заглушка", чтоб typescript не ругался
-    return 0
+export const sumFirstNumbers = (N: number): number => {
+	let result = 0
+	for (let i = 0; i <= N; i++) result += i
+	return result
 }
 
 // ...и "лапку" вверх!!!!
-
 
 // Д.З.:
 // 7. Функция-банкомат принимает параметром целое натуральное число (сумму).
@@ -68,9 +60,22 @@ export function sumFirstNumbers(N: number): number {
 // const banknotes = [1000, 500, 100, 50, 20, 10, 5, 2, 1].
 // Считаем, что количество банкнот каждого номинала не ограничено
 
+export const getBanknoteList = (amountOfMoney: number): number[] => {
+	const banknotes = [1000, 500, 100, 50, 20, 10, 5, 2, 1]
+	const result = []
+	let i = 0
 
-export function getBanknoteList(amountOfMoney: number): Array<number> {
-    //...здесь пишем код.
-    // В return стоит "заглушка", чтоб typescript не ругался
-    return [1]
+	while (i < banknotes.length) {
+		const countBanknote = Math.floor(amountOfMoney / banknotes[i])
+
+		if (countBanknote >= 1 && amountOfMoney) {
+			result.push(banknotes[i])
+			amountOfMoney -= banknotes[i]
+			i = 0
+		} else {
+			i++
+		}
+	}
+
+	return result
 }
