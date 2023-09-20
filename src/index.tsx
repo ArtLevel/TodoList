23ad44c4312b79499b07f313795b679f6331c17a
ArtLevel <algitIT@rambler.ru>
2023-09-20 19:13:43 +0300

    Create AppWithReducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import './index.css';
-import App from './App';
-import { createRoot } from 'react-dom/client';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import './index.css'
+import { createRoot } from 'react-dom/client'
+import * as serviceWorker from './serviceWorker'
+import AppWithUseReducer from './AppWithUseReducer'
 
-const container  = document.getElementById('root') as HTMLElement
-const root = createRoot(container);
-root.render(<App />);
+const container = document.getElementById('root') as HTMLElement
+const root = createRoot(container)
+root.render(<AppWithUseReducer />)
 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
