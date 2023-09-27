271de3fd2a8b73882b53cb8688511c3ffac60694
ArtLevel <algitIT@rambler.ru>
2023-09-27 19:07:50 +0300

    Fix optimization

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -31,40 +31,40 @@ function AppWithRedux() {
 	const tasks = useSelector<AppRootStateType, TasksStateType>(state => state.tasks)
 	const dispatch = useDispatch()
 
-	function removeTask(id: string, todolistId: string) {
+	const removeTask = useCallback((id: string, todolistId: string) => {
 		const action = removeTaskAC(id, todolistId)
 		dispatch(action)
-	}
+	}, [dispatch])
 
 	const addTask = useCallback((title: string, todolistId: string) => {
 		const action = addTaskAC(title, todolistId)
 		dispatch(action)
-	}, [])
+	}, [dispatch])
 
-	function changeStatus(id: string, isDone: boolean, todolistId: string) {
+	const changeStatus = useCallback((id: string, isDone: boolean, todolistId: string) => {
 		const action = changeTaskStatusAC(id, isDone, todolistId)
 		dispatch(action)
-	}
+	}, [dispatch])
 
-	function changeTaskTitle(id: string, newTitle: string, todolistId: string) {
+	const changeTaskTitle = useCallback((id: string, newTitle: string, todolistId: string) => {
 		const action = changeTaskTitleAC(id, newTitle, todolistId)
 		dispatch(action)
-	}
+	}, [dispatch])
 
-	function changeFilter(value: FilterValuesType, todolistId: string) {
+	const changeFilter = useCallback((value: FilterValuesType, todolistId: string) => {
 		const action = changeTodolistFilterAC(todolistId, value)
 		dispatch(action)
-	}
+	}, [dispatch])
 
-	function removeTodolist(id: string) {
+	const removeTodolist = useCallback((id: string) => {
 		const action = removeTodolistAC(id)
 		dispatch(action)
-	}
+	}, [dispatch])
 
-	function changeTodolistTitle(id: string, title: string) {
+	const changeTodolistTitle = useCallback((id: string, title: string) => {
 		const action = changeTodolistTitleAC(id, title)
 		dispatch(action)
-	}
+	}, [dispatch])
 
 	const addTodolist = useCallback((title: string) => {
 		const action = addTodolistAC(title)
