f6ef3695fa3b6c2b7768d1c838baaf2fd6a9d297
ArtLevel <algitIT@rambler.ru>
2023-10-05 20:23:10 +0300

    Task with CallStack

--- src/1.js
+++ src/1.js
@@ -1,19 +1,66 @@
-setTimeout(function a() {
-  console.log("t1");
-}, 1000);
-
-setTimeout(function c() {
-  console.log("t2");
-}, 500);
-
-setTimeout(function c() {
-  console.log("t3");
-}, 0);
-
-function d() {
-  console.log("d");
-}
-
-d();
+// setTimeout(function a() {
+//   console.log("t1");
+// }, 1000);
+//
+// setTimeout(function c() {
+//   console.log("t2");
+// }, 500);
+//
+// setTimeout(function c() {
+//   console.log("t3");
+// }, 0);
+//
+// function d() {
+//   console.log("d");
+// }
+//
+// d();
 
 // callstack, micro, animation, macro
+
+// function a() {
+//   console.log("a");
+// }
+
+// function b() {
+//   console.log("b");
+// }
+//
+// function c() {
+//   console.log("c");
+// }
+//
+// a();
+//
+// setTimeout(function timer() {
+//   console.log("timeout");
+// }, 2000);
+//
+// b();
+// c();
+
+// function a() {
+//   console.log("a");
+// }
+//
+// function b() {
+//   console.log("b");
+// }
+//
+// function c() {
+//   console.log("c");
+// }
+//
+// a();
+//
+// new Promise((res, rej) => {
+//   console.log("create promise");
+//   setTimeout(() => {
+//     res(console.log("timeout"));
+//   }, 5000);
+// });
+//
+// b();
+// c();
+
+// a create promise b c timeout
