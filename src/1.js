6e4c7fcbc98209dc6fae1735d0f06626cb49da31
ArtLevel <algitIT@rambler.ru>
2023-10-05 20:45:58 +0300

    New task

--- src/1.js
+++ src/1.js
@@ -64,3 +64,50 @@
 // c();
 
 // a create promise b c timeout
+
+// setTimeout(() => {
+//   console.log(1);
+// });
+//
+// new Promise((resolve, reject) => {
+//   console.log(2);
+//   resolve();
+// }).then(() => {
+//   console.log(3);
+// });
+//
+// console.log(4);
+
+// -------------------------------------------------
+
+setTimeout(() => {
+  console.log("s1");
+}, 0);
+
+setTimeout(() => {
+  console.log("s2");
+}, 1000);
+
+new Promise((res, rej) => {
+  console.log("p1");
+  res();
+  console.log("p2");
+}).then(() => {
+  console.log("p3");
+});
+
+console.log("w1");
+
+async function test1() {
+  console.log("a1");
+  await test2();
+  console.log("a2");
+}
+
+async function test2() {
+  console.log("a3");
+}
+
+test1();
+
+console.log("w2");
