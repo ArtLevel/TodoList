042a5a9143ac4fd63b074b1b10ef9d6e57bd30b2
ArtLevel <algitIT@rambler.ru>
2023-10-06 19:00:37 +0300

    Lesson complete

--- src/1.js
+++ src/1.js
@@ -112,37 +112,37 @@
 //
 // console.log("w2");
 
-console.log(1);
-
-setTimeout(() => {
-  console.log(2);
-  Promise.resolve().then(() => {
-    console.log(3);
-  });
-});
-
-new Promise((res, rej) => {
-  console.log(4);
-  res(5);
-}).then((data) => {
-  console.log(data);
-  Promise.resolve()
-    .then(() => {
-      console.log(6);
-    })
-    .then(() => {
-      console.log(7);
-
-      setTimeout(() => {
-        console.log(8);
-      }, 0);
-    });
-});
-
-setTimeout(() => {
-  console.log(9);
-});
-
-console.log(10);
+// console.log(1);
+//
+// setTimeout(() => {
+//   console.log(2);
+//   Promise.resolve().then(() => {
+//     console.log(3);
+//   });
+// });
+//
+// new Promise((res, rej) => {
+//   console.log(4);
+//   res(5);
+// }).then((data) => {
+//   console.log(data);
+//   Promise.resolve()
+//     .then(() => {
+//       console.log(6);
+//     })
+//     .then(() => {
+//       console.log(7);
+//
+//       setTimeout(() => {
+//         console.log(8);
+//       }, 0);
+//     });
+// });
+//
+// setTimeout(() => {
+//   console.log(9);
+// });
+//
+// console.log(10);
 
 // 1 4 10
