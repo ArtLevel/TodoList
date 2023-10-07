c7b9a8e6700818b139be592317d4c904bca33b03
ArtLevel <algitIT@rambler.ru>
2023-10-07 12:29:38 +0300

    Added AddItemForm.stories.tsx

--- src/stories/AddItemForm.stories.tsx
+++ src/stories/AddItemForm.stories.tsx
@@ -1,10 +1,13 @@
 import { AddItemForm } from '../AddItemForm'
+import { action } from '@storybook/addon-actions'
 
 export default {
 	title: 'AddItemForm',
 	component: AddItemForm
 }
 
+const callback = action("Button 'add' pressed")
+
 export const AddItemFormBaseExample = () => {
-	return <AddItemForm addItem={(title) => console.log(title)} />
+	return <AddItemForm addItem={callback} />
 }
