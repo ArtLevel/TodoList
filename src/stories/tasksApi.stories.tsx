157bbf0c588b6dc613c03c2cbb9a802badc8b096
ArtLevel <algitIT@rambler.ru>
2023-11-06 12:37:56 +0300

    HW Completed

--- src/stories/tasksApi.stories.tsx
+++ src/stories/tasksApi.stories.tsx
@@ -32,7 +32,7 @@ export const DeleteTasks = () => {
 	const [state, setState] = useState<any>(null)
 	useEffect(() => {
 		const todoListId = '3ec8a43b-78fc-4227-a9e9-8bfbf1a5b90a'
-		const taskId = '222ef72a-72bf-4979-bee4-9917782b19d1'
+		const taskId = '256c31d1-9007-49fb-9a94-93211997f3be'
 		todoListsAPI.deleteTask(todoListId, taskId)
 			.then(res => {
 				setState(res.data)
@@ -44,8 +44,8 @@ export const DeleteTasks = () => {
 export const UpdateTasksTitle = () => {
 	const [state, setState] = useState<any>(null)
 	useEffect(() => {
-		const todoListId = 'e38a8616-995f-43dd-9a46-1061b58276b4'
-		const taskId = '256c31d1-9007-49fb-9a94-93211997f3be'
+		const todoListId = '3ec8a43b-78fc-4227-a9e9-8bfbf1a5b90a'
+		const taskId = '096b1a5d-d833-4a4e-97cc-d546e8be6e41'
 		todoListsAPI.updateTask(todoListId, taskId)
 			.then(res => {
 				setState(res.data)
