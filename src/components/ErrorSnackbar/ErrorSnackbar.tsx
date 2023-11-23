f5d80a2cf281068eab462bb05777405556b60ffa
ArtLevel <algitit@rambler.ru>
2023-11-23 19:51:39 +0300

    Refactoring ErrorSnackbar

--- src/components/ErrorSnackbar/ErrorSnackbar.tsx
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Snackbar from '@mui/material/Snackbar'
 import MuiAlert, { AlertProps } from '@mui/material/Alert'
+import { useAppDispatch, useAppSelector } from '../../app/store'
+import { setAppErrorAC } from '../../app/app-reducer'
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
 	props, ref) {
@@ -8,18 +10,22 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
 })
 
 export function ErrorSnackbar() {
-	const [open, setOpen] = useState(true)
+	const error = useAppSelector(s => s.app.error)
+	const dispatch = useAppDispatch()
+
+	const isOpen = error !== null
 
 	const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
 		if (reason === 'clickaway') {
 			return
 		}
-		setOpen(false)
+		dispatch(setAppErrorAC(null))
 	}
+
 	return (
-		<Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+		<Snackbar open={isOpen} autoHideDuration={6000} onClose={handleClose}>
 			<Alert onClose={handleClose} severity='error' sx={{ width: '100%' }}>
-				Error message 😠
+				{error}
 			</Alert>
 		</Snackbar>
 	)
